ce98301e34ab0f3be92d837fbfa96278376fd813
Erik <65004236+Vikingu-del@users.noreply.github.com>
2024-03-29 08:21:20 +0000

    made some changes in the photos and description content

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Erik Seferi",
+  title: "Electronics & Software Engineer",
+  email: "rk.seferi@gmail.com",
+  gitHub: "Vikingu-del ",
+  instagram: "erik_seferi",
+  linkedIn: "Erik Seferi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,30 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineer student studying at 42 Wolfsburg. I have a passion for creating user-friendly, accessible, and inclusive web applications. I am always looking for new ways to improve my skills and learn new technologies.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C",
+  "C++",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "HTML",
+  "CSS",
+  "SASS",
+  "Bootstrap",
+  "Git",
+  "Python",
+  "Docker",
+  "shell",
+  "Linux",
+  "SQL",
 ];
 
 /**
@@ -45,7 +56,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I am a quick learner and enjoy working in a team environment. Give me a challenge and I will find a solution.";
 
 const About = () => {
   return (
