42ea1d234a84af97b6d8ac343245927bd2b24b7f
Erik <65004236+Vikingu-del@users.noreply.github.com>
2024-03-29 10:04:44 +0000

    made the arrow animate

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,7 +33,7 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      <img src={arrowSvg} className="bounceArrow" style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
     </section>
   );
